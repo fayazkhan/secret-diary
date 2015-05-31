7beb94f50371679550d138049c34ba2a798aadd2
Fayaz Yusuf Khan <fayaz@weavedin.com>
2015-05-31 12:25:52 +0530

    specify all requirements

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(
     name="secret-diary",
     version="2.0",
     packages=find_packages(),
-    install_requires=["pysqlcipher"],
+    install_requires=["arrow", "docopt", "pysqlcipher", "sqlalchemy-utils"],
     scripts=['diary'])
