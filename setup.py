a9635843f7c724316cc548ccbbaffe104e010d9b
Fayaz Yusuf Khan <fayaz@weavedin.com>
2015-05-31 10:24:45 +0530

    specify requirements

--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ from setuptools import find_packages, setup
 
 setup(
     name="secret-diary",
-    version="0.1",
-    packages=find_packages())
+    version="2.0",
+    packages=find_packages(),
+    install_requires=["pysqlcipher"])
