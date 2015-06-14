676cec59b2a67060e7815a9b8aa79bf276e5c708
Fayaz Yusuf Khan <fayaz@weavedin.com>
2015-06-14 20:19:18 +0530

    test write from buffer

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
+import sys
 from unittest.mock import Mock, patch
 
 import arrow
+
 from diary import create_entry, Entry, main, show, write_from_buffer
 
 
@@ -31,12 +33,25 @@ def test_write_from_buffer():
     assert entry.content == 'helloworld'
 
 
-@patch('diary.docopt', return_value={'<file>': 'test.db'})
+@patch('diary.docopt', return_value={'<file>': 'test.db', 'show': True})
 @patch('diary.getpass', return_value='password')
 @patch('diary.create_database_session')
 @patch('diary.show')
-def test_main_show(show, create_database_session, _):
+def test_main_show(show, create_database_session, *_):
     session = create_database_session.return_value
     main()
     create_database_session.assert_called_once_with('test.db', 'password')
     show.assert_called_once_with(session)
+
+
+@patch('diary.docopt', return_value={
+    '<file>': 'test.db', 'show': False,
+    'write': True, '--create': False, '--message': None})
+@patch('diary.getpass', return_value='password')
+@patch('diary.create_database_session')
+@patch('diary.write_from_buffer')
+def test_main_write_from_buffer(write_from_buffer,
+                                create_database_session, *_):
+    session = create_database_session.return_value
+    main()
+    write_from_buffer.assert_called_once_with(session, sys.stdin)
