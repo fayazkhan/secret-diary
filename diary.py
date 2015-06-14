5f1a39be6930e94762aa8d87535f1128f9ce28c6
Fayaz Yusuf Khan <fayaz@weavedin.com>
2015-06-14 19:45:02 +0530

    refactor all functionality

--- diary.py
+++ diary.py
@@ -15,7 +15,7 @@ from getpass import getpass
 import sys
 
 import arrow
-from sqlalchemy_utils import ArrowType
+from sqlalchemy_utils import ArrowType, force_instant_defaults
 from docopt import docopt
 from pysqlcipher3 import dbapi2 as sqlcipher
 from sqlalchemy import create_engine, Column, UnicodeText
@@ -27,35 +27,52 @@ def main():
     arguments = docopt(__doc__, version="Fayaz's diary 2.0")
     file = arguments['<file>']
     password = getpass(prompt='Database password: ')
-    engine = create_engine(
-        'sqlite+pysqlcipher://:{password}@/{database}'.format(
-            database=file, password=password), module=sqlcipher)
-    session = sessionmaker(bind=engine)()
+    session = create_database_session(file, password)
     if arguments['show']:
         show(session)
     elif arguments['write']:
         if arguments['--create']:
-            Base.metadata.create_all(bind=engine)
-        write(session, message=arguments['--message'])
+            Base.metadata.create_all(bind=session)
+        if arguments['--message']:
+            create_entry(session, message=arguments['--message'])
+        else:
+            write_from_buffer(session, sys.stdin)
+
+
+def create_database_session(file, password):
+    engine = create_engine(
+        'sqlite+pysqlcipher://:{password}@/{database}'.format(
+            database=file, password=password), module=sqlcipher)
+    session = sessionmaker(bind=engine)()
+    return session
 
 
 def show(session):
     for entry in session.query(Entry):
-        print(entry.updated.humanize(), entry.content)
+        display_row(entry.updated.humanize(), entry.content)
 
 
-def write(session, message=None):
+def display_row(time, text):
+    print(time, text)
+
+
+def create_entry(session, message=''):
     now = arrow.utcnow()
-    entry = Entry(created=now, updated=now, content=message or '')
+    entry = Entry(created=now, updated=now, content=message)
     session.add(entry)
     session.commit()
-    if not entry.content:
-        for line in sys.stdin:
-            entry.content += line
-            entry.updated = arrow.utcnow()
-            session.commit()
+    return entry
 
 
+def write_from_buffer(session, buffer):
+    entry = create_entry(session)
+    for line in buffer:
+        entry.content += line
+        entry.updated = arrow.utcnow()
+        session.commit()
+
+
+force_instant_defaults()
 Base = declarative_base()
 
 
@@ -64,7 +81,7 @@ class Entry(Base):
     __tablename__ = 'entry'
     created = Column(ArrowType, primary_key=True)
     updated = Column(ArrowType, nullable=False)
-    content = Column(UnicodeText, nullable=False)
+    content = Column(UnicodeText, nullable=False, default='')
 
 
 if __name__ == '__main__':
