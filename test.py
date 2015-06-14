4eeb22c6bae21fcf37dccb2202aeac3a1aa52d98
Fayaz Yusuf Khan <fayaz@weavedin.com>
2015-06-14 19:44:37 +0530

    add detailed tests

--- test.py
+++ test.py
@@ -1,8 +1,35 @@
-from mock import Mock
+from unittest.mock import Mock, patch
 
-from diary import show
+import arrow
+from diary import create_entry, Entry, main, show, write_from_buffer
 
 
 def test_show():
-    show(session)
-    print.assert_called_once_with(humanized_time, content)
+    content = 'test'
+    time = arrow.now()
+    session = Mock(query=Mock(return_value=[Entry(
+        updated=time, content=content)]))
+    with patch('diary.display_row') as display_row:
+        show(session)
+    display_row.assert_called_once_with(time.humanize(), content)
+
+
+def test_create_entry():
+    session = Mock()
+    entry = create_entry(session, message='test')
+    session.add.assert_called_once_with(entry)
+    session.commit.assert_called_once_with()
+    assert entry.content == 'test'
+
+
+def test_write_from_buffer():
+    session = Mock()
+    buffer = ['hello', 'world']
+    entry = Entry()
+    with patch('diary.create_entry', return_value=entry):
+        write_from_buffer(session, buffer)
+    assert entry.content == 'helloworld'
+
+
+def test_main():
+    main()
