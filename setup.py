131f464a37fcfed55518dbe36b1e73d59779964b
Fayaz Yusuf Khan <fayaz@weavedin.com>
2015-06-14 11:33:44 +0530

    use pysqlcipher3 directly

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(
     name="secret-diary",
     version="2.0",
     packages=find_packages(),
-    install_requires=["arrow", "docopt", "pysqlcipher", "sqlalchemy-utils"],
+    install_requires=["arrow", "docopt", "pysqlcipher3", "sqlalchemy-utils"],
     scripts=['diary'])
