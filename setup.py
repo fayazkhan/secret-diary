37d95fb1328eb88090682039f4495e2e21fcac86
Fayaz Yusuf Khan <fayaz@weavedin.com>
2015-06-14 16:48:19 +0530

    add test runners

--- setup.py
+++ setup.py
@@ -6,4 +6,6 @@ setup(
     version="2.0",
     packages=find_packages(),
     install_requires=["arrow", "docopt", "pysqlcipher3", "sqlalchemy-utils"],
-    scripts=['diary'])
+    scripts=['diary'],
+    test_suite='nose.collector',
+    setup_requires=['coverage', 'nose'])
