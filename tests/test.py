a02382626ba5031f516fa9aee6ff58eef11bb67d
Fayaz Yusuf Khan <fayaz@weavedin.com>
2015-06-21 12:31:34 +0530

    refactor tests

--- tests/test.py
+++ tests/test.py
@@ -31,27 +31,3 @@ def test_write_from_buffer():
     with patch('diary.create_entry', return_value=entry):
         write_from_buffer(session, buffer)
     assert entry.content == 'helloworld'
-
-
-@patch('diary.docopt', return_value={'<file>': 'test.db', 'show': True})
-@patch('diary.getpass', return_value='password')
-@patch('diary.create_database_session')
-@patch('diary.show')
-def test_main_show(show, create_database_session, *_):
-    session = create_database_session.return_value
-    main()
-    create_database_session.assert_called_once_with('test.db', 'password')
-    show.assert_called_once_with(session)
-
-
-@patch('diary.docopt', return_value={
-    '<file>': 'test.db', 'show': False,
-    'write': True, '--create': False, '--message': None})
-@patch('diary.getpass', return_value='password')
-@patch('diary.create_database_session')
-@patch('diary.write_from_buffer')
-def test_main_write_from_buffer(write_from_buffer,
-                                create_database_session, *_):
-    session = create_database_session.return_value
-    main()
-    write_from_buffer.assert_called_once_with(session, sys.stdin)
