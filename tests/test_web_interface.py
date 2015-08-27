5eb215a5fc46a51fb65d29e68ff67a06171408e0
Fayaz Yusuf Khan <fayaz@weavedin.com>
2015-08-27 19:00:07 +0530

    test the application factory routine

--- tests/test_web_interface.py
+++ tests/test_web_interface.py
@@ -1,2 +1,10 @@
-def test_listing():
-    pass
+from unittest.mock import patch, sentinel
+
+from diary.web import application_factory
+
+
+@patch('diary.web.Flask', return_value=sentinel.app)
+def test_webapp_initialization(Flask):
+    app = application_factory()
+    Flask.assert_called_once_with('diary.web')
+    assert app == sentinel.app
