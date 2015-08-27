8cde4076f79b24da275eee18a3df3a4964751623
Fayaz Yusuf Khan <fayaz@weavedin.com>
2015-08-27 20:22:31 +0530

    flask and flask-admin required

--- setup.py
+++ setup.py
@@ -3,8 +3,13 @@ from setuptools import find_packages, setup
 
 setup(
     name="secret-diary",
-    version="2.0",
+    version="3.0",
     packages=find_packages(),
     entry_points={'console_scripts': ['diary = diary:main']},
-    install_requires=["arrow", "docopt", "pysqlcipher3", "sqlalchemy-utils"],
+    install_requires=["arrow",
+                      "docopt",
+                      "flask",
+                      "flask-admin",
+                      "pysqlcipher3",
+                      "sqlalchemy-utils"],
     test_suite='nose.collector')
