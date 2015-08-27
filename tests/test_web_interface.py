f8034181c9dd9b880ee0805fe2c6566260e060e1
Fayaz Yusuf Khan <fayaz@weavedin.com>
2015-08-27 19:16:36 +0530

    test flask_admin connection

--- tests/test_web_interface.py
+++ tests/test_web_interface.py
@@ -8,3 +8,9 @@ def test_webapp_initialization(Flask):
     app = application_factory()
     Flask.assert_called_once_with('diary.web')
     assert app == sentinel.app
+
+
+@patch('diary.web.Admin')
+def test_admin_initialzed(Admin):
+    app = application_factory()
+    Admin.assert_called_once_with(app)
